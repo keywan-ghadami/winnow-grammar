f58d327b083c5be5aa8e539672fe097d8716bd53
keywan.ghadami <keywan.ghadami@gmail.com>
2026-02-11 03:35:42 +0000

    Add left recursion test

---
+++ tests/left_recursion.rs
@@ -0,0 +1,33 @@
+use winnow::prelude::*;
+use winnow_grammar::grammar;
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Expr {
+    Num(u32),
+    Add(Box<Expr>, Box<Expr>),
+}
+
+grammar! {
+    grammar LeftRec {
+        rule expr -> Expr =
+            l:expr "+" r:term -> { Expr::Add(Box::new(l), Box::new(r)) }
+          | t:term -> { t }
+
+        rule term -> Expr =
+            n:uint -> { Expr::Num(n) }
+    }
+}
+
+#[test]
+fn test_left_recursion() {
+    let input = "1 + 2 + 3";
+    let result = LeftRec::parse_expr.parse(input).unwrap();
+    // (1 + 2) + 3
+    assert_eq!(
+        result,
+        Expr::Add(
+            Box::new(Expr::Add(Box::new(Expr::Num(1)), Box::new(Expr::Num(2)))),
+            Box::new(Expr::Num(3))
+        )
+    );
+}
