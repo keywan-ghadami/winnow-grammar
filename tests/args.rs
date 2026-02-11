5fc27f3913263cb559a9c66b7ab7f1f52e0887ec
keywan.ghadami <keywan.ghadami@gmail.com>
2026-02-11 04:45:25 +0000

    Add failing test for rule arguments

---
+++ tests/args.rs
@@ -0,0 +1,21 @@
+use winnow::prelude::*;
+use winnow::stream::LocatingSlice;
+use winnow_grammar::grammar;
+
+grammar! {
+    grammar Args {
+        rule main -> i32 = 
+            "start" v:value(10) -> { v }
+
+        rule value(offset: i32) -> i32 =
+            i:integer -> { i + offset }
+    }
+}
+
+#[test]
+fn test_args() {
+    let input = "start 5";
+    let input = LocatingSlice::new(input);
+    let result = Args::parse_main.parse(input).unwrap();
+    assert_eq!(result, 15);
+}
