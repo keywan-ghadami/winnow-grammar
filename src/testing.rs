00e21cc8b67485ce8c0525298c8842eef534cd07
Keywan Ghadami <keywan.ghadami@gmail.com>
2026-03-14 20:08:50 +0100

    add testcase macro again

--- src/testing.rs
+++ src/testing.rs
@@ -38,10 +38,10 @@ where
 #[macro_export]
 macro_rules! test_case {
     ($name:ident, rule: $rule:ident, { $($grammar:tt)* }, [ $(($input:expr, $($check:tt)*)),* $(,)? ]) => {
-        $crate::testing::test_case_impl! (
+        $crate::test_case_impl! (
             backend: {
                 grammar_macro: $crate::grammar,
-                test_trait: $crate::testing::WinnowTestExt<'_, _>,
+                test_trait: $crate::testing::WinnowTestExt,
                 parser_mut: mut
             },
             name: $name,
@@ -51,10 +51,10 @@ macro_rules! test_case {
         );
     };
     ($name:ident, { $($grammar:tt)* }, [ $(($input:expr, $($check:tt)*)),* $(,)? ]) => {
-        $crate::testing::test_case_impl! (
+        $crate::test_case_impl! (
             backend: {
                 grammar_macro: $crate::grammar,
-                test_trait: $crate::testing::WinnowTestExt<'_, _>,
+                test_trait: $crate::testing::WinnowTestExt,
                 parser_mut: mut
             },
             name: $name,
@@ -63,3 +63,86 @@ macro_rules! test_case {
         );
     };
 }
+
+/// Implementation of the test case logic.
+///
+/// This macro creates an outer module named after the test for isolation.
+///
+/// The entry point defaults to `parse_main` but can be customized via the `rule` parameter.
+#[macro_export]
+macro_rules! test_case_impl {
+    // Variant with explicit rule name
+    (
+        backend: {
+            grammar_macro: $grammar_macro:path,
+            test_trait: $test_trait:path,
+            parser_mut: $($parser_mut:ident)?
+        },
+        name: $name:ident,
+        rule: $rule:ident,
+        grammar: { $($grammar:tt)* },
+        cases: [ $( ($input:expr, $($check:tt)*) ),* $(,)? ]
+    ) => {
+        #[allow(non_snake_case)]
+        mod $name {
+            use paste::paste;
+            use $grammar_macro as grammar;
+            use $test_trait;
+
+            paste! {
+                grammar! { grammar [<$name _grammar>] { $($grammar)* } }
+            }
+
+            #[test]
+            fn run() {
+                paste!{
+                    macro_rules! run_check {
+                        ($inp:expr, val $expect:expr) => {
+                            #[allow(unused_mut)]
+                            let $($parser_mut)? parser = [<$name _grammar>]::[<parse_ $rule>];
+                            parser.parse_test($inp).assert_success_is($expect);
+                        };
+                        ($inp:expr, err $msg:expr) => {
+                            #[allow(unused_mut)]
+                            let $($parser_mut)? parser = [<$name _grammar>]::[<parse_ $rule>];
+                            parser.parse_test($inp).assert_failure_contains($msg);
+                        };
+                        ($inp:expr, check $closure:expr) => {
+                            #[allow(unused_mut)]
+                            let $($parser_mut)? parser = [<$name _grammar>]::[<parse_ $rule>];
+                            parser.parse_test($inp).assert_success_with($closure);
+                        };
+                    }
+                    $(
+                        run_check!($input, $($check)*);
+                    )*
+                }
+            }
+        }
+    };
+
+    // Variant without explicit rule name (defaults to 'main')
+    (
+        backend: {
+            grammar_macro: $grammar_macro:path,
+            test_trait: $test_trait:path,
+            parser_mut: $($parser_mut:ident)?
+        },
+        name: $name:ident,
+        grammar: { $($grammar:tt)* },
+        cases: [ $( ($input:expr, $($check:tt)*) ),* $(,)? ]
+    ) => {
+        $crate::test_case_impl! {
+            backend: {
+                grammar_macro: $grammar_macro,
+                test_trait: $test_trait,
+                parser_mut: $($parser_mut)?
+            },
+            name: $name,
+            rule: main, // Default rule name
+            grammar: { $($grammar)* },
+            cases: [ $(($input, $($check)*)),* ]
+        }
+    };
+}
+
