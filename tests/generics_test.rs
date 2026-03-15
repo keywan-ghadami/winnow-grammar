a7e3c48acc412a73d8265f0d7f4bddafaaccab9f
keywan.ghadami <keywan.ghadami@gmail.com>
2026-03-15 19:18:34 +0000

    wip generics

--- tests/generics_test.rs
+++ tests/generics_test.rs
@@ -5,16 +5,14 @@ use winnow_grammar::testing::WinnowTestExt;
 grammar! {
     grammar Generics {
         list<T>(item) -> Vec<T> =
-            "[" elements:item* "]" -> { elements }
-        pub main -> Vec<u32> = l:list(item=u32_parser) -> { l }
-
-        u32_parser -> u32 = i:u32 -> { i }
+            elements:item* -> { elements }
+        pub main -> Vec<u32> = l:list<u32>(u32) -> { l }
     }
 }
 
 #[test]
 fn test_generics() {
     Generics::parse_main()
-        .parse_test("[ 1 2 3 ]")
+        .parse_test("1 2 3")
         .assert_success_is(vec![1, 2, 3]);
 }
